95f6d03c070acdf1310da85430ac3289b6ba5d0c
hgreatrex <5038576+hgreatrex@users.noreply.github.com>
2021-08-30 03:54:59 -0400

    Lab 1 update
    All lab 1 files

---
+++ lab2tmp.R
@@ -0,0 +1,21 @@
+#### F. Adding pictures 
+
+9. Finally, go find a favorite picture/photo/screenshot.  Save it inside your lab 1 folder. 
+
+
+6. Leave another blank line and add a new heading called *"Adding Pictures"*.  Add a blank line afterwards too.
+
+
+7. Make sure the screenshot is in your Lab 1 folder. Now include a code chunk that looks similar to this, but with your user name/file name (note, it might be a .jpeg not a .png). When you run it, the screenshot should appear below the code chunk.
+
+
+```{r, echo=FALSE}
+knitr::include_graphics("GEOG364_Lab1_fig01_Picture.png")
+```
+
+Make sure the screenshot is in your Lab 1 folder. Now include a code chunk that looks similar to this, but with your user filename (note, it might be a .jpeg not a .png). When you run it, the screenshot should appear below the code chunk.  If it can't find it, ARE YOU RUNNING YOUR R PROJECT? (e.g. does it say lab 1 at the top of your screen?)
+
+
+\b
+<div style="margin-bottom:25px;">
+</div>
